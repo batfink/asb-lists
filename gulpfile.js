e8db86801fe24d5d32bff976c512016d85457ba8
Victor Nystad <vnystad@gmail.com>
2014-11-13 12:08:26 +0100

    jsonml, compression

--- gulpfile.js
+++ gulpfile.js
@@ -2,16 +2,30 @@ var gulp = require('gulp');
 var browserSync = require('browser-sync');
 var reload = browserSync.reload;
 var deploy = require('gulp-gh-pages');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+
+gulp.task('scripts', function() {
+  gulp.src('./lib/*.js')
+    .pipe(concat('all.js'))
+    .pipe(gulp.dest('./dist/'))
+});
+
 
 var dependencies = [
     'bower_components/tabletop/src/tabletop.js',
     'bower_components/es6-promise/promise.js',
-    'bower_components/marked/lib/marked.js',
-    'node_modules/document-register-element/build/document-register-element.js'
+    'node_modules/markdown/lib/markdown.js',
+    'bower_components/jsonml/jsonml-html.js'
 ];
 
 gulp.task('setup', function() {
     gulp.src(dependencies).pipe(gulp.dest('www/lib'));
+});
+
+gulp.task('compress', function() {
+    dependencies.push('www/script.js');
+    gulp.src(dependencies).pipe(concat('asb.all.min.js')).pipe(uglify()).pipe(gulp.dest('www/lib'));
 });
 
 gulp.task('default', ['setup'], function() {
